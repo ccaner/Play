2109bdf76731846172393d07b0a1aa7733b84581
Caner <canerakpinar@gmail.com>
2012-04-28 01:51:22 +0100

    improve

---
+++ resultsetmock/src/main/java/play/resultsetmock/annotations/RsColumn.java
@@ -0,0 +1,11 @@
+package play.resultsetmock.annotations;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: akpinarc
+ * Date: 4/27/12
+ * Time: 11:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RsColumn {
+}
---
+++ resultsetmock/src/test/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: akpinarc
+ * Date: 4/28/12
+ * Time: 1:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
